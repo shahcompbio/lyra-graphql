f7f1a636f42f041a9ba36ab722187165068d30ea
Samantha Leung <sleung@bccrc.ca>
2018-05-24 14:32:19 -0700

    Add schema and resolvers for tree node querying

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { GraphQLServer } = require("graphql-yoga");
 const fetch = require("node-fetch");
 const elasticsearch = require("elasticsearch");
-
+const util = require("util");
 let client = new elasticsearch.Client({
   host: "localhost:9200",
   log: "trace"
@@ -10,6 +10,9 @@ let client = new elasticsearch.Client({
 const typeDefs = `
 type Query {
   dashboards: [Dashboard!]!
+  analysis(analysis: String!, dashboard: String!): Analysis
+  treeRoot(analysis: String!): Node
+  treeNode(analysis: String!, cellID: String!): Node
 }
 
 type Dashboard {
@@ -24,6 +27,23 @@ type Analysis {
   segsIndex: String!
   treeIndex: String!
 }
+
+type Node {
+  id: String!
+  parent: String!
+  index: Int!
+  maxIndex: Int!
+  maxHeight: Int!
+  children: [NodeChild!]!
+}
+
+type NodeChild {
+  id: String!
+  index: Int!
+  maxIndex: Int!
+  maxHeight: Int!
+}
+
 `;
 
 const allAnalysisQuery = () => ({
@@ -51,6 +71,66 @@ const resolvers = {
         id: bucket.key,
         analyses: results.hits.hits.map(hit => hit["_source"])
       }));
+    },
+    async analysis(_, { analysis, dashboard }) {
+      const results = await client.search({
+        index: "tree_analysis",
+        body: {
+          query: {
+            bool: {
+              must: [
+                {
+                  term: {
+                    dashboard: {
+                      value: dashboard
+                    }
+                  }
+                },
+                {
+                  term: {
+                    analysis_id: {
+                      value: analysis
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        }
+      });
+
+      return results.hits.hits[0]["_source"];
+    },
+
+    async treeRoot(_, { analysis }) {
+      const results = await client.search({
+        index: analysis + "_tree",
+        body: {
+          size: 1,
+          query: {
+            bool: {
+              filter: [{ term: { parent: "root" } }]
+            }
+          }
+        }
+      });
+
+      return results.hits.hits[0];
+    },
+
+    async treeNode(_, { analysis, cellID }) {
+      const results = await client.search({
+        index: analysis + "_tree",
+        body: {
+          size: 1,
+          query: {
+            bool: {
+              filter: [{ term: { cell_id: cellID } }]
+            }
+          }
+        }
+      });
+      return results.hits.hits[0];
     }
   },
   Dashboard: {
@@ -67,6 +147,37 @@ const resolvers = {
     description: root => root.description,
     segsIndex: root => root.segs_index,
     treeIndex: root => root.tree_index
+  },
+
+  Node: {
+    id: root => root["_source"].cell_id,
+    parent: root => root["_source"].parent,
+    index: root => root["_source"].heatmap_order,
+    maxIndex: root => root["_source"].max_index,
+    maxHeight: root => root["_source"].max_height,
+    children: root => {
+      return root["_source"].children.map(async child => {
+        const results = await client.search({
+          index: root["_index"],
+          body: {
+            size: 1,
+            query: {
+              bool: {
+                filter: [{ term: { cell_id: child } }]
+              }
+            }
+          }
+        });
+        return results.hits.hits[0]["_source"];
+      });
+    }
+  },
+
+  NodeChild: {
+    id: root => root.cell_id,
+    index: root => root.heatmap_order,
+    maxIndex: root => root.max_index,
+    maxHeight: root => root.max_height
   }
 };
 
