8092c47018f04e7e9250775e76f2349bb6f51742
Samantha Leung <sleung@bccrc.ca>
2018-06-05 16:41:43 -0700

    Add resolver and schema for querying bins for clones

--- src/segs.js
+++ src/segs.js
@@ -6,6 +6,7 @@ export const schema = gql`
   extend type Query {
     chromosomes(analysis: String!): [Chromosome]
     segs(analysis: String!, indices: [Int!]!): [SegRow]
+    cloneSegs(analysis: String!, range: [Int!]!): [Seg]
   }
 
   type Chromosome {
@@ -29,6 +30,149 @@ export const schema = gql`
   }
 `;
 
+async function getIDsForIndices(analysis, indices) {
+  const results = await client.search({
+    index: `ce00_${analysis.toLowerCase()}_tree`,
+    body: {
+      size: 50000,
+      sort: [
+        {
+          heatmap_order: {
+            order: "asc"
+          }
+        }
+      ],
+      query: {
+        bool: {
+          must: [
+            {
+              terms: {
+                heatmap_order: indices
+              }
+            }
+          ]
+        }
+      }
+    }
+  });
+
+  return results;
+}
+
+async function getIDsForRange(analysis, range) {
+  const results = await client.search({
+    index: `ce00_${analysis.toLowerCase()}_tree`,
+    body: {
+      size: 50000,
+      query: {
+        bool: {
+          must: [
+            {
+              range: {
+                heatmap_order: {
+                  gte: range[0],
+                  lte: range[1]
+                }
+              }
+            }
+          ]
+        }
+      }
+    }
+  });
+  return results;
+}
+
+const getBinQuery = ids => {
+  const rangeQuery = Array.apply(null, { length: 500 }).map(
+    (_, i) => ({
+      from: i === 0 ? 0 : 500000 * i + 1,
+      to: (i + 1) * 500000
+    }),
+    []
+  );
+
+  return {
+    size: 0,
+    query: {
+      bool: {
+        must: [
+          {
+            terms: {
+              cell_id: ids
+            }
+          }
+        ]
+      }
+    },
+    aggs: {
+      chromosomes: {
+        terms: {
+          field: "chrom_number",
+          size: 50
+        },
+        aggs: {
+          bins: {
+            histogram: {
+              field: "start",
+              interval: 500000
+            },
+            aggs: {
+              state: {
+                terms: {
+                  field: "state",
+                  size: 1
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  };
+};
+
+const getSegsForChromosome = (id, bins) => {
+  const convertBinsToSegs = (currSeg, allSegs, bins) => {
+    if (bins.length === 0) {
+      return [...allSegs, currSeg];
+    } else {
+      const [firstBin, ...restBin] = bins;
+
+      if (hasSameState(currSeg, firstBin)) {
+        return convertBinsToSegs(
+          mergeSegs(currSeg, firstBin),
+          allSegs,
+          restBin
+        );
+      } else {
+        return convertBinsToSegs(
+          convertToSeg(firstBin),
+          [...allSegs, currSeg],
+          restBin
+        );
+      }
+    }
+  };
+
+  const getBinState = bin => bin.state.buckets[0].key;
+
+  const hasSameState = (seg, bin) => seg.state === getBinState(bin);
+
+  const mergeSegs = (seg, bin) => ({
+    ...seg,
+    end: bin.key + 500000
+  });
+
+  const convertToSeg = bin => ({
+    chrom_number: id,
+    start: bin.key === 0 ? bin.key : bin.key + 1,
+    end: bin.key + 500000,
+    state: getBinState(bin)
+  });
+  const [firstBin, ...restBin] = bins;
+  return convertBinsToSegs(convertToSeg(firstBin), [], restBin);
+};
 export const resolvers = {
   Query: {
     async chromosomes(_, { analysis }) {
@@ -66,32 +210,33 @@ export const resolvers = {
     },
 
     async segs(_, { analysis, indices }) {
-      const results = await client.search({
-        index: `ce00_${analysis.toLowerCase()}_tree`,
-        body: {
-          size: 50000,
-          sort: [
-            {
-              heatmap_order: {
-                order: "asc"
-              }
-            }
-          ],
-          query: {
-            bool: {
-              must: [
-                {
-                  terms: {
-                    heatmap_order: indices
-                  }
-                }
-              ]
-            }
-          }
-        }
-      });
+      const results = await getIDsForIndices(analysis, indices);
 
       return results.hits.hits;
+    },
+
+    async cloneSegs(_, { analysis, range }) {
+      const idResults = await getIDsForRange(analysis, range);
+      const ids = idResults.hits.hits.map(record => record["_source"].cell_id);
+
+      const cloneBins = await client.search({
+        index: `cl00_${analysis.toLowerCase()}_bins`,
+        body: getBinQuery(ids)
+      });
+
+      const results = cloneBins.aggregations.chromosomes.buckets.reduce(
+        (results, chromosome) => [
+          ...results,
+          ...getSegsForChromosome(chromosome.key, chromosome.bins.buckets)
+        ],
+        []
+      );
+
+      const roundedResults = results.map(result => ({
+        ...result,
+        state: Math.round(result.state)
+      }));
+      return results;
     }
   },
 
