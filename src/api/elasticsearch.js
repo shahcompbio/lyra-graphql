73896ec2bebdca75986b251a2e8fccab4ff6cc85
Samantha Leung <sleung@bccrc.ca>
2018-06-13 16:32:37 -0700

    Add env HOST option

--- src/api/elasticsearch.js
+++ src/api/elasticsearch.js
@@ -1,6 +1,8 @@
 const elasticsearch = require("elasticsearch");
+
+const HOST = process.env.HOST || "localhost";
 const client = new elasticsearch.Client({
-  host: "localhost:9200"
+  host: HOST + ":" + "9200"
 });
 
 export default client;
