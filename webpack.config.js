9f5d79f0839474835d236432577a74ee2a9ef887
redpanda-cat <leungs1@mskcc.org>
2019-01-25 15:08:14 -0500

    Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false, // if you don't put this is, __dirname
+    __filename: false // and __filename return blank or /
+  },
+  externals: [nodeExternals()], // Need this to avoid error when working with Express
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
