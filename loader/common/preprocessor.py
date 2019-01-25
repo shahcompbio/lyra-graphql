b8254976db285633ed93139318912971a4bf7160
Spenca <svatrt@bccrc.ca>
2019-01-25 14:47:03 -0500

    Clean up preprocessor.py

--- loader/common/preprocessor.py
+++ loader/common/preprocessor.py
@@ -8,29 +8,28 @@ class Preprocessor(object):
         self.newick_file = newick_file
         self.matching_required = matching_required
 
-    def trim_prefixes_and_add_root(self):
+    def trim_prefixes(self, newick_string):
+        newick_string = newick_string.replace('cell_', '')
+        return newick_string
+
+    def add_root(self, newick_string):
+        newick_string = re.sub(';', 'root;', newick_string)
+        return newick_string
+
+    def match_ids(self, newick_string):
+        newick_string = re.sub('(SA[0-9]+)[a-zA-Z]+', r'\1', newick_string)
+        return newick_string
+
+    def preprocess(self):
         with open(self.newick_file, 'r+') as newick_file:
             newick_string = newick_file.read().replace('\n', '')
 
-            newick_string = re.sub(';', 'root;', newick_string)
-            newick_string = newick_string.replace('cell_', '')
+            newick_string = self.trim_prefixes(newick_string)
+            newick_string = self.add_root(newick_string)
+
+            if self.matching_required:
+                newick_string = self.match_ids(newick_string)
 
             newick_file.seek(0)
             newick_file.write(newick_string)
             newick_file.truncate()
-
-    def match_ids(self):
-        with open(self.newick_file, 'r+') as newick_file:
-            newick_string = newick_file.read()
-
-            newick_string = re.sub('(SA[0-9]+)[a-zA-Z]+', r'\1', newick_string)
-
-            newick_file.seek(0)
-            newick_file.write(newick_string)
-            newick_file.truncate()
-
-    def preprocess(self):
-        self.trim_prefixes_and_add_root()
-
-        if self.matching_required:
-            self.match_ids()
