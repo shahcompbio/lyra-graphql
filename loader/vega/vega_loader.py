594561aa4234b9540bd9dd56f0c2b6c36a96626b
vbojilova <37880309+vbojilova@users.noreply.github.com>
2019-02-13 11:32:10 -0500

    Update vega_loader.py
    Revert to three arguments and output written to mount directory

--- loader/vega/vega_loader.py
+++ loader/vega/vega_loader.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 import sys
-sys.path.append('/lyra-graphql/loader')
+import json
 from common.tree_loader import TreeLoader
 
 
 def main():
     data_type = sys.argv[1]
-    
+    file_name = sys.argv[3]
+
     if data_type == "tree":
         tree_loader = TreeLoader()
         file_path = sys.argv[2]
         json_data = tree_loader.load_file_as_json(analysis_file=file_path)
 
-        print(json_data)
+        with open('./mount/'+file_name, 'w') as outfile:
+            json.dump(json_data, outfile)
         sys.stdout.flush()
-
-
 
 if __name__ == '__main__':
     main()
