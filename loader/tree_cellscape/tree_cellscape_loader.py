ac530a66aee9500d40712800ea39aac172b48309
vbojilova <37880309+vbojilova@users.noreply.github.com>
2019-02-18 14:06:40 -0500

    Update tree_cellscape_loader.py
    Converting dict to str

--- loader/tree_cellscape/tree_cellscape_loader.py
+++ loader/tree_cellscape/tree_cellscape_loader.py
@@ -51,7 +51,7 @@ def load_analysis_entry(args, yaml_data):
     analysis_loader.import_file(record, dashboard_type)
     logging.info("Analysis entry loaded")
     sys.stdout.flush()
-    print(record+"-AnalysisDone")
+    print(str(record)+"-AnalysisDone")
     sys.stdout.flush()
 
 def load_tree_data(args, yaml_data):
@@ -76,7 +76,7 @@ def load_tree_data(args, yaml_data):
         tree_edges=yaml_data.get_file_paths("tree_edges")
     )
     sys.stdout.flush()
-    print(yaml_data.get_file_paths("tree")+"-TreeDone")
+    print(str(yaml_data.get_file_paths("tree"))+"-TreeDone")
     sys.stdout.flush()
 
 def load_segs_data(args, yaml_data):
@@ -106,7 +106,7 @@ def load_segs_data(args, yaml_data):
                 analysis_file=seg_file
             )
             sys.stdout.flush()
-            print(seg_file+"-SegDone")
+            print(str(seg_file)+"-SegDone")
             sys.stdout.flush()
 
 
